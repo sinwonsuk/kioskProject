d11c129d3f9c427d09d72d87591e1cbe780fc3b6
sinwonsuk <onerpg3023@gmail.com>
2024-12-26 22:01:05 +0900

    orderItem 부분 추가

---
+++ kioskProject/kioskProject/Model/OrderItemModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kioskProject.Model
+{
+    internal class OrderItemModel : INotifyPropertyChanged
+    {
+		private int price;
+
+		public int Price
+		{
+			get { return price; }
+			set { price = value;
+                  NotifyPropertyChanged(nameof(price));
+            }
+		}
+
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+        private int quantity;
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value;
+                  NotifyPropertyChanged(nameof(quantity));
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
--- kioskProject/kioskProject/ViewModel/MainViewModel.cs
+++ kioskProject/kioskProject/ViewModel/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using kioskProject.Model;
+﻿using GalaSoft.MvvmLight.Command;
+using kioskProject.Model;
+using kioskProject.View;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -7,18 +9,27 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace kioskProject.ViewModel
 {
     class MainViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<Model.Model> Models { get; set; }
+        public ObservableCollection<OrderItemModel> orderItemModels { get; set; }
 
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        public ICommand? StartClick { get; set; }
 
 
         public MainViewModel()
         {
+            StartClick = new RelayCommand<object>(test);
+
+            orderItemModels = new ObservableCollection<OrderItemModel>();
+
             Models = new ObservableCollection<Model.Model>
             {
                 new Model.Model { Name = "test1", Price = 500 },
@@ -26,11 +37,11 @@ namespace kioskProject.ViewModel
                 new Model.Model { Name = "test3", Price = 700 },
                 new Model.Model { Name = "test4", Price = 800 },
                 new Model.Model { Name = "test5", Price = 800 },
-                 new Model.Model { Name = "test5", Price = 800 },
-                  new Model.Model { Name = "test5", Price = 800 },
-                   new Model.Model { Name = "test5", Price = 800 },
-                    new Model.Model { Name = "test5", Price = 800 },
-                  new Model.Model { Name = "test5", Price = 800 }
+                new Model.Model { Name = "test5", Price = 800 },
+                new Model.Model { Name = "test5", Price = 800 },
+                new Model.Model { Name = "test5", Price = 800 },
+                new Model.Model { Name = "test5", Price = 800 },
+                new Model.Model { Name = "test5", Price = 800 }
             };
         }
 
@@ -41,6 +52,24 @@ namespace kioskProject.ViewModel
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        void test(object e)
+        {
+            Model.Model? ad = e as Model.Model;
+
+            //for (int i = 0; i < orderItemModels.Count; i++)
+            //{
+            //    if (orderItemModels[i].Name == ad.Name)
+            //    {
+            //        orderItemModels[i].
+            //    }
+            //}
+
+
+            OrderItemModel model = new OrderItemModel{Name = ad.Name, Price = ad.Price};
+            orderItemModels.Add(model);
+        }
+
     }
 
   
